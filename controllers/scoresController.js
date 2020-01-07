ffbf1946f66bd5546e76345e7a12aecf425ff485
Sarah Arnold <digipet007@gmail.com>
2020-01-07 11:43:18 -0800

    fixed models path bug

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -1,33 +1,37 @@
-const db = require("../models");
+const db = require("../models/House");
 
 // Defining methods for the scoresController
 module.exports = {
   findAll: function(req, res) {
-    db.House.find(req.query)
+    console.log("find function in scoresController.js");
+    db.find(req.query)
       .sort({ currScore: -1 })
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => {
+        // console.log(dbModel);
+        res.json(dbModel);
+      })
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     console.log("findById function in scoresController.js");
-    db.House.findById(req.params.id)
+    db.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     console.log("create function in scoresController.js");
-    db.House.create(req.body)
+    db.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     console.log("update function in scoresController.js");
-    db.House.findOneAndUpdate({ _id: req.params.id }, req.body) //route for updating all?
+    db.findOneAndUpdate({ _id: req.params.id }, req.body) //route for updating all?
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
     // },
     // remove: function(req, res) {
-    //   db.House.findById({ _id: req.params.id })
+    //   db.findById({ _id: req.params.id })
     //     .then(dbModel => dbModel.remove())
     //     .then(dbModel => res.json(dbModel))
     //     .catch(err => res.status(422).json(err));
