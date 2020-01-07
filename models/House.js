a528571d7be4d832aa1771af3772800dcf57f213
Sarah Arnold <digipet007@gmail.com>
2020-01-07 09:34:36 -0800

    added length validation and further simplified model

--- models/House.js
+++ models/House.js
@@ -3,17 +3,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
-  gameInfo: {
+  // gameInfo: {
+  //   type: String,
+  //   required: true,
+  //   unique: true
+  // },
+  playerName: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, "must provide a player name"],
+    unique: true,
+    maxlength: 32
   },
-  players: [
-    {
-      name: { type: String, required: true, unique: true },
-      currScore: { type: Number, default: 0 }
-    }
-  ]
+  currScore: { type: Number, default: 50 },
+  date: { type: Date, default: Date.now }
+
+  // players: [
+  //   {
+  //     name: { type: String, required: true, unique: true },
+  //     currScore: { type: Number, default: 0 }
+  //   }
+  // ]
 });
 
 const House = mongoose.model("House", gameSchema);
