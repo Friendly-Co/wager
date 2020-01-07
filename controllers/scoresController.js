6d546d98144712eab8ee5b8aa7401556ef18ae4e
Sarah Arnold <digipet007@gmail.com>
2020-01-07 14:51:17 -0800

    added console.log

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -33,6 +33,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
+    console.log("remove function in scoresController.js");
     console.log(req.params.id);
     db.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
