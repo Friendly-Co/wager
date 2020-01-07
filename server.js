d742c902bc8a136fa869a1847e66dae418631faa
Sarah Arnold <digipet007@gmail.com>
2020-01-07 11:46:13 -0800

    added seeded database to server

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "mongodb://localhost/reactbettingscores",
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`API Server now listening on PORT ${PORT}`);
 });
