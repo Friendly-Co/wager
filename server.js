e737cb90fecc820939075434c22e0de953d90aaf
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2020-01-07 20:22:48 -0800

    reconfigured server to work with socketio and express

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
+const http = require("http");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const socketIo = require("socket.io");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', socket => {
+  console.log('User connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +39,10 @@ mongoose.connect(
 );
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`API Server now listening on PORT ${PORT}`);
-});
+// app.listen(PORT, function() {
+//   console.log(`API Server now listening on PORT ${PORT}`);
+// });
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
