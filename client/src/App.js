f71871ae16a313ac79c9048e2f097e599356c1ea
Sarah Arnold <digipet007@gmail.com>
2020-01-09 11:33:27 -0800

    renamed/rerouted AdminLeaderboard to AdminGame

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import AdminLeaderboard from "./pages/AdminLeaderboard";
+import AdminGame from "./pages/AdminGame";
 import Login from "./pages/Login";
 import User from "./pages/User/User";
 import "./App.css";
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Switch>
           {/* <Route exact path="/" component={} /> */}
-          <Route exact path="/adminleaderboard" component={AdminLeaderboard} />
+          <Route exact path="/admingame" component={AdminGame} />
           <Route exact path="/user" component={User} />
           <Route exact path="/login" component={Login} />
         </Switch>
