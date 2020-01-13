5a072cd10132c46984c323539d182db0bb9a83b4
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2020-01-13 14:47:48 -0800

    making guess state a constant element on the user page

--- client/src/components/Score/Score.js
+++ client/src/components/Score/Score.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Score(props) {
     return(
         <div>
-            <div id="scoreBox">
+            <div id="scoreBox"> 
             <h3>User:<strong> {props.user} </strong> Score: <strong>{props.score}</strong></h3>
             </div>
         </div>
