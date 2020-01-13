c5bb52ca7537319347b43e8a96b5e2d7ebf1c497
Sarah Arnold <digipet007@gmail.com>
2020-01-13 11:52:13 -0800

    sending playerName

--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -15,16 +15,14 @@ let username;
 let tempboard = [];
 
 class User extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-        score,
-        guess,
-        username,
-        setModalShow: false,
-        leaderboard: []
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      score,
+      guess,
+      username,
+      setModalShow: false,
+      leaderboard: []
     };
 
     this.socket = io("localhost:3001");
@@ -32,67 +30,66 @@ class User extends Component {
     this.socket.on("RECIEVE_MESSAGE", function(data) {});
 
     this.sendGuess = ev => {
-        // ev.preventDefault();
-        this.socket.emit('SEND_MESSAGE', {
-            username: this.state.username,
-            currentGuess: this.state.guess,
-        });
-        // this.setState({guess: ''});
-        console.log(guess);
-        // this.setState({})
-    }
-}
+      // ev.preventDefault();
+      this.socket.emit("SEND_MESSAGE", {
+        playerName: this.state.username,
+        currentGuess: this.state.guess
+      });
+      // this.setState({guess: ''});
+      console.log(guess);
+      // this.setState({})
+    };
+  }
 
-componentDidMount() {
+  componentDidMount() {
     username = this.props.match.params.username;
     this.setState({
-        username: username
-    })
+      username: username
+    });
     console.log(username);
     this.loadScore();
     this.loadLeaderboard();
     console.log(this.scoreSeed);
   }
 
-    
-      // Loads score and sets them to this.state.scores
-      loadScore = () => {
-        API.getPlayerScore(username)
-          .then(res =>
-            this.setState({
-              score: res.data[0].currScore
-            })
-          )
-          .catch(err => console.log(err));
-      };
-
-      loadLeaderboard = () => {
-        API.getScores()
-        .then(res => {
-            tempboard = [];
-            for (let i = 0; i < 10; i++) {
-                // console.log(res.data[i]);
-              tempboard.push(res.data[i]);
-            }
-            this.setState({leaderboard: tempboard})
-            console.log(this.state.leaderboard);
+  // Loads score and sets them to this.state.scores
+  loadScore = () => {
+    API.getPlayerScore(username)
+      .then(res =>
+        this.setState({
+          score: res.data[0].currScore
         })
-          .catch(err => console.log(err));  
-    }
+      )
+      .catch(err => console.log(err));
+  };
 
-    // function that updates guess state with onClick
-// guessUpdate = (value) => {
-//     this.setState({ guess: value});
-// };
+  loadLeaderboard = () => {
+    API.getScores()
+      .then(res => {
+        tempboard = [];
+        for (let i = 0; i < 10; i++) {
+          // console.log(res.data[i]);
+          tempboard.push(res.data[i]);
+        }
+        this.setState({ leaderboard: tempboard });
+        console.log(this.state.leaderboard);
+      })
+      .catch(err => console.log(err));
+  };
 
-toggleModalOn = () => {
+  // function that updates guess state with onClick
+  // guessUpdate = (value) => {
+  //     this.setState({ guess: value});
+  // };
+
+  toggleModalOn = () => {
     this.loadLeaderboard();
-    this.setState({setModalShow: true});
-};
+    this.setState({ setModalShow: true });
+  };
 
-toggleModalOff = () => {
-    this.setState({setModalShow: false})
-};
+  toggleModalOff = () => {
+    this.setState({ setModalShow: false });
+  };
 
   // function that updates guess state with onClick
   guessUpdate = value => {
@@ -114,48 +111,43 @@ toggleModalOff = () => {
     });
   };
 
-render() {
+  render() {
     const sortedLeaderboard = this.state.leaderboard;
     const tableBody = sortedLeaderboard.map((player, index) => (
-        <tr key={player._id}>
-            <td>{index + 1} </td>
-            <td>{player.playerName} </td>
-            <td>{player.currScore} </td>
-        </tr>
+      <tr key={player._id}>
+        <td>{index + 1} </td>
+        <td>{player.playerName} </td>
+        <td>{player.currScore} </td>
+      </tr>
     ));
     return (
-        <div>
-            <Logo/>
-            <Score
-                score={this.state.score}
-            />
-            <GuessState
-                onChange={this.sendGuess()} 
-                guess={this.state.guess}
-            />
-            <GuessButtons
-            guessUpdate={this.guessUpdate}
-            toggleModalOn={this.toggleModalOn}
-            />
-            <LeaderModal
-            username={this.state.username}
-            score={this.state.score}
-            show={this.state.setModalShow}
-            leaderboard={tableBody}
-            onHide={() => this.toggleModalOff()}
-            />
-            {/* <CorrectModal
+      <div>
+        <Logo />
+        <Score score={this.state.score} />
+        <GuessState onChange={this.sendGuess()} guess={this.state.guess} />
+        <GuessButtons
+          guessUpdate={this.guessUpdate}
+          toggleModalOn={this.toggleModalOn}
+        />
+        <LeaderModal
+          username={this.state.username}
+          score={this.state.score}
+          show={this.state.setModalShow}
+          leaderboard={tableBody}
+          onHide={() => this.toggleModalOff()}
+        />
+        {/* <CorrectModal
             score={this.state.score}
             show={this.state.setModalShow}
             onHide={() => this.toggleModalOff()}
             /> */}
-            {/* <HaltModal 
+        {/* <HaltModal 
             show={this.state.setModalShow}
             onHide={() => this.toggleModalOff()}
             /> */}
-        </div>
-    )
-}
+      </div>
+    );
+  }
 }
 
 export default User;
