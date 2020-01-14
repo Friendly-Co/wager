97aed62a95cc11288e18d2b79c02ca37a4666d52
Sarah Arnold <digipet007@gmail.com>
2020-01-13 20:26:33 -0800

    function to increase scores depending on user and admin answers

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -3,7 +3,7 @@ const db = require("../models/House");
 // Defining methods for the scoresController
 module.exports = {
   findAll: function(req, res) {
-    console.log("find function in scoresController.js");
+    console.log("findAll function in scoresController.js");
     db.find(req.query)
       .sort({ currScore: -1 })
       .then(dbModel => {
@@ -28,10 +28,51 @@ module.exports = {
   create: function(req, res) {
     console.log("create function in scoresController.js");
     console.log(req.body);
+    //save guesses
     if (req.body.currentGuess) {
       db.findOneAndUpdate(
         { playerName: req.body.playerName },
         { currentGuess: req.body.currentGuess }
+      )
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+      console.log("guess saved!");
+    }
+    // calculate scores and save
+    else if (Object.keys(req.body[req.body.length - 1]).includes("answer")) {
+      console.log("this has an answer in here!");
+      var answer = req.body.pop();
+      answer = answer.answer.toUpperCase();
+      console.log("answer: ");
+      console.log(answer); //Run
+      console.log("req.body: ");
+      console.log(req.body); //[ { playerName: 'Dexter', currentGuess: 'PASS' } ]
+      const rightAnswers = req.body.filter(index =>
+        index.currentGuess.includes(answer)
+      );
+
+      console.log("rightAnswers: ");
+      console.log(rightAnswers);
+      var toAdd = 0;
+      switch (answer) {
+        case "RUN" || "PASS":
+          toAdd = 2;
+          break;
+        case "KICK":
+          toAdd = 1;
+          break;
+        case "TURNOVER":
+          toAdd = 10;
+          break;
+        default:
+          toAdd = 0;
+      }
+      console.log("toAdd: ");
+      console.log(toAdd);
+      db.updateMany(
+        { currentGuess: answer },
+        { $inc: { currScore: +toAdd } },
+        { multi: true }
       )
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
