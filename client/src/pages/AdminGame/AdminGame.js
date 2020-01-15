95dd6d6743153ec37230e1e9cf9043ecdf6ef089
Sarah Arnold <digipet007@gmail.com>
2020-01-15 14:15:07 -0800

    rendering calculated scores

--- client/src/pages/AdminGame/AdminGame.js
+++ client/src/pages/AdminGame/AdminGame.js
@@ -18,8 +18,7 @@ class AdminGame extends Component {
 
     //when socket receives a current bet from a user, update the scoreSeed state
     this.socket.on("RECIEVE_MESSAGE", function(data) {
-      //if there is a currentGuess, render to the page- may need to change
-      // if (data.currentGuess !== " " && data.playerName !== " ") {
+      //if there is a currentGuess, render to the page
       if (data.playerName) {
         console.log(data.playerName);
         addUserInfo(data);
@@ -56,7 +55,7 @@ class AdminGame extends Component {
           return { scoreSeed };
         } else {
           var scoreSeed = [...state.scoreSeed];
-          scoreSeed[playerIndex].currentGuess = data.currentGuess; //Note: updating state of array needs to be immutable
+          scoreSeed[playerIndex].currentGuess = data.currentGuess;
           playerIndex = -1;
           return { scoreSeed };
         }
@@ -84,44 +83,16 @@ class AdminGame extends Component {
   handleAnswer = value => {
     const houseAnswer = { answer: value };
     const toSend = this.state.scoreSeed.concat(houseAnswer);
-    // console.log(value);
-    // console.log(value);
-    // new Promise(function(resolve, reject) {
     API.saveScore(toSend).then(res => {
       console.log("scores saved");
       console.log("after that, ...");
       console.log(res.data);
-      // .then(res => {
-      this.setState(
-        state => {
-          // var scoreSeed = [...state.scoreSeed];
-          // console.log(state.scoreSeed);
-          state.scoreSeed = []; //Note: updating state of array needs to be immutable
-          // });
-        },
-        function() {
-          this.setState(state => {
-            state.scoreSeed = [...res.data];
-          });
-        }
-      );
-      // });
+      this.setState({
+        scoreSeed: [...res.data]
+      });
+      console.log("this.state.scoreSeed is now: ");
+      console.log(this.state.scoreSeed);
     });
-    // .catch(err => console.log(err))
-    // .then(function() {
-    // return
-    // API.getScores().then(response => {
-    //   console.log("here is your data :");
-    //   console.log(response.data); //sometimes returns data too soon- I know because currentGuess should be empty
-    console.log("this.state.scoreSeed is now: ");
-    console.log(this.state.scoreSeed);
-    // });
-    // .catch(err => console.log(err));
-    // resolve(result);
-    // this.setState({ answer: value });
-    // console.log(this.state.scoreSeed); //undefined
-    // });
-    // });
   };
 
   deleteAllPlayers = () => {
