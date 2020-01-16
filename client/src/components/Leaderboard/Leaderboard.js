0271873861dbdc2e6778e673cdc2a7c4921e1781
Sarah Arnold <digipet007@gmail.com>
2020-01-16 08:16:44 -0800

    added fallback to pull from database if new players login but never bet

--- client/src/components/Leaderboard/Leaderboard.js
+++ client/src/components/Leaderboard/Leaderboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import DeleteBtn from "../DeleteBtn";
 import { List, ListItem } from "../List";
+import { get } from "mongoose";
 // import io from "socket.io-client";
 
 class Leaderboard extends Component {
@@ -37,21 +38,41 @@ class Leaderboard extends Component {
   };
 
   render(props) {
+    //If no scoreSeed props are coming from the Admin page, render from the database
+    // This is a fallback for bugs related to user and admin page reloads or loosing players
     var renderFrom = this.state.dbScores;
-    if (this.props.scoreSeed.length) {
+    var dbScoresLength = 0;
+    for (let key in this.state.dbScores) {
+      dbScoresLength++;
+    }
+    console.log("Here is the length of dbScores");
+    console.log(dbScoresLength);
+    console.log("Here is the length of scoreSeed");
+    console.log(this.props.scoreSeed.length);
+    //If scoreSeed is longer than dbScores, pull from the database to update state, but still render from the scoreSeed to show updated guesses
+    if (dbScoresLength < this.props.scoreSeed.length) {
+      console.log(
+        "scoreSeed is longer than dbScores- Checking database for updates"
+      );
+      this.getFromDb();
+    }
+    if (this.props.scoreSeed.length >= dbScoresLength) {
+      console.log("rendering from scoreSeed. This should be the default");
       renderFrom = this.props.scoreSeed;
+      console.log("here's what I have in dbScores though: ");
+      console.log(this.state.dbScores);
     } else {
-      // if there is are no current scores in props, pull ONCE from the database and rendeer those
-      // this.getFromDb(); //bug:  this will constatntly load in the beginning of the game.
-
+      // If the dbScores state is longer than this.props.scoreSeed, something is wrong - display from the database
+      console.log(
+        "Something may be wrong. I have more players in the database than in the scoreSeed props. Did we gain a player who hasn't guessed?? Did we loose a player somehow?? Rendering from this.props.dbScores. FYI current guesses will always be one round behind."
+      );
       renderFrom = this.state.dbScores;
     }
     return (
       <div>
         <h1>Player Scores</h1>
         <div className="container">
-          {/* need to fix: if the length is zero OR there is no current score in props, render from the database */}
-          {this.props.scoreSeed.length ? (
+          {this.props.scoreSeed.length || this.state.dbScores ? (
             <List>
               {renderFrom.map(score => {
                 return (
@@ -69,8 +90,8 @@ class Leaderboard extends Component {
             <h3>No Players to Display</h3>
           )}
           <DeleteBtn onClick={() => this.props.deleteAllPlayers()}>
-          END GAME/Clear Data
-        </DeleteBtn>
+            END GAME/Clear Data
+          </DeleteBtn>
         </div>
       </div>
     );
