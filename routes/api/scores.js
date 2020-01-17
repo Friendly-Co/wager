cf235cec6bd8bc451dcee9d6e1adf4a484ed07e9
Sarah Arnold <digipet007@gmail.com>
2020-01-17 15:55:41 -0800

    minor changes in order

--- routes/api/scores.js
+++ routes/api/scores.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const scoresController = require("../../controllers/scoresController");
 
+router
+  .route("/score/:playerName")
+  .get(scoresController.findByName)
+  .put(scoresController.update);
+
+// Matches with "/api/scores/:id" this is the id of the player
+router.route("/:id").get(scoresController.findById);
+// .delete(scoresController.remove);
+
 // Matches with "/api/scores"
 router
   .route("/")
@@ -8,15 +17,5 @@ router
   .post(scoresController.create)
   .put(scoresController.update)
   .delete(scoresController.removeAll);
-
-// Matches with "/api/scores/:id" this is the id of the player
-router.route("/:id").get(scoresController.findById);
-// .put(scoresController.update);
-// .delete(scoresController.remove);
-
-router
-  .route("/score/:playerName")
-  .get(scoresController.findByName)
-  .put(scoresController.update);
 
 module.exports = router;
