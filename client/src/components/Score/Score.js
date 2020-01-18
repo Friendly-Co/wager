87fe3ca4dbf5c7c43aeca868d2453afa6c2f93ab
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2020-01-18 10:45:24 -0800

    user names and score now displaying

--- client/src/components/Score/Score.js
+++ client/src/components/Score/Score.js
@@ -6,7 +6,7 @@ function Score(props) {
     <div>
       <div id="scoreBox">
         {/* Possible change, esp if componentDidMount loadScores function doesn't fire if props.score.length, render props.score, else, socket or something is down, so pull the score from the database */}
-        <h3 id="scorepage">
+        <h3 className="scoreName" id="scorepage">
           User:<strong> {props.user} </strong> Score:
           <strong>{props.score}</strong>
         </h3>
