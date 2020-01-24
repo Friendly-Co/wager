d47b34dc81b29d3d4e9e498aa5924221b0ba11b1
Sarah Arnold <digipet007@gmail.com>
2020-01-24 09:29:35 -0800

    renaming some functions

--- client/src/utils/AdminAPI.js
+++ client/src/utils/AdminAPI.js
@@ -2,31 +2,31 @@ import axios from "axios";
 
 export default {
   // Gets all players and scores
-  getAllAdmin: function() {
-    console.log("getAllAdmin from AdminAPI.js");
+  getAllGames: function() {
+    console.log("getAllGames from AdminAPI.js");
     return axios.get("/api/admin");
   },
   // Deletes all admin
-  deleteAllAdmin: function() {
-    console.log("deleteAllAdmin from AdminAPI.js");
+  deleteAll: function() {
+    console.log("deleteAll from AdminAPI.js");
     return axios.delete("/api/admin/");
   },
   //   // Gets the player with the given id
   //   getScore: function(id) {
   //     return axios.get("/api/admin/" + id);
   //   },
-  // Gets the admin with the given adminName
-  getAdminInfo: function(adminName) {
-    console.log("getAdminInfo from AdminAPI.js- adminName:");
-    return axios.get("/api/admin/login" + adminName);
+  // Gets the admin with the given adminName and gameId
+  getAdminInfo: function(id) {
+    console.log("getAdminInfo from AdminAPI.js- game id");
+    return axios.get("/api/admin/login", id);
   },
   // Deletes the player with the given id
   // deletePlayer: function(id) {
   //   return axios.delete("/api/scores/" + id);
   // },
   // Saves admin to the database
-  saveAdmin: function(toSave) {
-    console.log("saveAdmin from AdminAPI.js- toSave:");
+  saveGame: function(toSave) {
+    console.log("saveGame from AdminAPI.js- toSave:");
     console.log(toSave);
     // return axios.post("/api/admin/login", toSave);
     return axios.post("/api/admin/adminname", toSave);
