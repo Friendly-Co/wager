91ece2da7a2cebd424c190da45c1014f39dfc0a6
Tellis <teellis20@gmail.com>
2020-02-23 23:00:48 -0800

    undo modal created, modal font color update

--- client/src/components/CorrectModal/CorrectModal.js
+++ client/src/components/CorrectModal/CorrectModal.js
@@ -13,7 +13,9 @@ function CorrectModal(props) {
         >
           <Modal.Header>
             <Modal.Title id="contained-modal-title-vcenter">
-                <img alt="football field" src="https://media.istockphoto.com/vectors/american-football-field-background-illustration-vector-id478540218?k=6&m=478540218&s=612x612&w=0&h=a_29HZiRtjFZwA1io1IkcI7G3HbS-NMNx9QN8o9MoKc="></img>
+            <div className="imgContainer">
+                <img className="modalImg" alt="football field" src="https://media.istockphoto.com/vectors/american-football-field-background-illustration-vector-id478540218?k=6&m=478540218&s=612x612&w=0&h=a_29HZiRtjFZwA1io1IkcI7G3HbS-NMNx9QN8o9MoKc="></img>
+                </div>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
@@ -30,7 +32,7 @@ function CorrectModal(props) {
         
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={props.onHide}>Close</Button>
+            <Button id="leaderBttn" onClick={props.onHide}>Close</Button>
           </Modal.Footer>
         </Modal>
       );
